a6b6d2bd10566491b51fcdcfc706b5188f8d126b
Opilane Techno+TLN <opilane@tthk.ee>
2026-09-03 14:13:11 +0300

    SpaceshipDto

---
+++ ShopTAR25/ShopTAR25.ApplicationServices/Services/SpaceshipServices.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShopTAR25.ApplicationServices.Services
+{
+    internal class SpaceshipServices
+    {
+    }
+}
---
+++ ShopTAR25/ShopTAR25.Core/ServiceInterface/ISpaceshipServices.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShopTAR25.Core.ServiceInterface
+{
+    internal interface ISpaceshipServices
+    {
+    }
+}
