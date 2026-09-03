8dffafccc4af509a2def61c883c3812a1162a704
Opilane Techno+TLN <opilane@tthk.ee>
2026-09-03 13:42:14 +0300

    DbContextLeiutamine

---
+++ ShopTAR25/ShopTAR25.Data/ShopTAR25Context.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShopTAR25.Data
+{
+    //teha sellest classist DBContext, et saaks andmebaasi kasutada
+    public class ShopTAR25Context : DbContext
+    {
+        public ShopTAR25Context(DbContextOptions<ShopTAR25Context> options) : base(options)
+        { }
+    }
+}
