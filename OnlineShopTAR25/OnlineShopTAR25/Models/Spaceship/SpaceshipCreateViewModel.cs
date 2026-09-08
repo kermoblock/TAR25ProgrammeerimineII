5927c10f580eb3bfa2aee2a87673a0e2593157da
Opilane Techno+TLN <opilane@tthk.ee>
2026-09-08 14:12:22 +0300

    SpaceshipCreate (puudu view)

--- OnlineShopTAR25/OnlineShopTAR25/Models/Spaceship/SpaceshipCreateViewModel.cs
+++ OnlineShopTAR25/OnlineShopTAR25/Models/Spaceship/SpaceshipCreateViewModel.cs
@@ -2,5 +2,13 @@
 {
     public class SpaceshipCreateViewModel
     {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Classification { get; set; } = string.Empty;
+        public DateTime? BuiltDate { get; set; }
+        public int? Crew { get; set; }
+        public int? EnginePower { get; set; }
+        public DateTime? CreateAt { get; set; }
+        public DateTime? ModifiedAt { get; set; }
     }
 }
